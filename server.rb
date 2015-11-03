78add0082c1b97174223ffcb4fc82a1d66ecdb1b
gonzamvita <gonzamv@gmail.com>
2015-11-03 14:19:28 +0100

    app started

---
+++ server.rb
@@ -0,0 +1,30 @@
+require "sinatra"
+
+
+get "/" do
+  erb(:home)
+end
+
+# get "/about" do
+#   "Learning Sinatra in Ironhack"
+# end
+
+# get "/author" do
+#   @greeting = "Hello World!!"
+#   @thetime = Time.now()
+#   @ingredients = ["cheese", "pepperoni", "mushrooms"]
+#   erb(:author)
+# end
+
+# get "/users/:username" do
+#   @username = params[:username]
+
+#   erb(:user_profile)
+# end
+
+# get "/hours/ago/:shift" do
+#   @shift = params[:shift].to_i
+#   @adjusted_datetime = (Time.now - @shift*60*60).to_datetime
+
+#   erb(:hours_ago)
+# end
